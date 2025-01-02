9099153d45da16cc20f7143abcf9e4f4d44b4f12
Chris Bushelman <chrisbushelman@gmail.com>
2025-01-02 12:00:22 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -166,6 +166,4 @@ def get_top_results():
         return jsonify({"error": f"Unexpected error: {e}"}), 500
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
-
+    app.run(debug=True)
